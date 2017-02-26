f719f378dbc32b894001c0150533d61a902e3544
bob <bob@bobsgame.com>
2017-02-26 04:57:35 -0500

    remove unused lib sources

--- lib/win32demo_connecting_facebook_v01_src/README.txt
+++
@@ -1,3 +0,0 @@
-load win32Demo.sln in to visual studio express 2008 
-thanks
-meiry242@gmail.com
--- lib/win32demo_connecting_facebook_v01_src/win32Host/win32Host/New Folder/resource.h
+++
@@ -1,8 +0,0 @@
-#ifndef IDC_STATIC
-#define IDC_STATIC (-1)
-#endif
-
-#define IDD_DIALOG1                             100
-#define IDC_BUTTON2                             1000
-#define IDC_BUTTON1                             1002
-#define IDC_EDIT1                               1003
